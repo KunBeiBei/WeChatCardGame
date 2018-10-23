ddedc3056e97a67ff980cf0d8b511d18739c2c91
liang <liang@example.com>
2018-10-23 15:05:41 +0800

    兑换奖品

--- pages/exchange/exchange.js
+++ pages/exchange/exchange.js
@@ -3,25 +3,19 @@ Page({
     gift: [
       
     ],
-    isShow: true,
-    ys: 'two',
-    sy: 'bt',
-    //已抢光样式
-    // isShow: fasle,
-    // ys: 'ys',
-    // sy: 'sy',
     openid: ''
   },
   onLoad: function (obj) {
     var that = this
-    that.openid = wx.getStorageSync('openId');
+    var openid = wx.getStorageSync('openId');
     wx.request({
-      // method: 'POST',
-      // data: {
-      //   'openId': openId
-      // },
+      method: 'POST',
+      data: {
+        'openId': openid
+      },
       url: 'https://www.yuebaoyuan.com.cn/wx/public/index.php/api2/getGift',
       success: function (res) {
+        console.log(res);
         that.setData({
           gift: res.data
         })
@@ -29,28 +23,32 @@ Page({
     })
   },
   exchange: function (res) {//点击兑换方法，res.target.dataset.id为礼物id
-    var that = this
-    that.id = res.target.dataset.id
-    wx.request({
-      url: 'https://www.yuebaoyuan.com.cn/wx/public/index.php/api2/Exchange',
-      method: 'POST',
-      data: {
-        'openId': that.openid,
-        'id': that.id
-      },
-      success: function(obj){
-        if(obj.data.status==200){
-          wx.navigateTo({
-            url: '../gift/gift?code='+obj.data.code
-          })
-        }else{
-          wx.showToast({
-            'title': obj.data.mes,
-            'icon': 'none',
-            'image': '../images/exit.png'
-          })
+    var openid = wx.getStorageSync('openId');
+    var that = this;
+    that.id = res.target.dataset.id;
+    var id = parseInt(res.target.dataset.id) - 1;
+    if (that.data.gift[id].isShow == 1){
+      wx.request({
+        url: 'https://www.yuebaoyuan.com.cn/wx/public/index.php/api2/Exchange',
+        method: 'POST',
+        data: {
+          'openId': openid,
+          'id': that.id
+        },
+        success: function (obj) {
+          if (obj.data.status == 200) {
+            wx.navigateTo({
+              url: '../gift/gift?code=' + obj.data.code
+            })
+          } else {
+            wx.showToast({
+              'title': obj.data.mes,
+              'icon': 'none',
+              'image': '../images/exit.png'
+            })
+          }
         }
-      }
-    })      
+      })  
+    }  
   }
 })
